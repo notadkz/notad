6fc59c1195261385a3cc02439f16405d65320ce2
notad <phancongchau80fm@gmail.com>
2024-09-12 16:46:20 +0700

    abc

--- app/FE/Body.tsx
+++ app/FE/Body.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Body = () => {
-  return <div className="bg-blue-600 text-white h-[1000px]">BODY</div>;
+  return <div className="bg-blue-600 text-white h-[1000px]">BODYasd</div>;
 };
 
 export default Body;
